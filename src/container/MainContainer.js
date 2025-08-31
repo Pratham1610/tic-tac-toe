d1dd786492939e376624aa72f5118aa075f5535c
Pratham Singhal <pratham.1singhal678@gmail.com>
2025-08-31 17:53:39 +0530

    Initial commit

---
+++ src/container/MainContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainContainer = (props) => {
+    return (
+        <div className="main-container">
+            hello this is main container
+        </div>
+    );
+}
+
+export default MainContainer;
---
+++ src/network/Readme.md
@@ -0,0 +1,2 @@
+# There will be future need for this folder
+### yet there is no online socket connectivity in this project
